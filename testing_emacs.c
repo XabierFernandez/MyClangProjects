baa46aeb34c504282cd5bdf816874d5eaac0eb27
Xabier Fernandez <xabier.fernandez@outlook.com>
2022-05-03 22:47:06 +0200

    updated with new exercises

---
+++ testing_emacs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+  int num =6;
+  int num1 =0;
+  printf("Number: %d\n",5);
+
+  printf("num: %d\n",num);
+  printf("num1: %d\n",num1);
+  num1++;
+  printf("num1: %d\n",num1);
+  return 0;
+}
