5244df2e63eb49161041ade08d40562f8bd23925
Xabier Fernandez <xabier.fernandez@outlook.com>
2022-05-19 21:01:48 +0200

    updated with new examples

--- mathLibraryExample.c
+++ mathLibraryExample.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 
-/*
-1.41421356                                                                      
-2.7182818285                                                                    
-gcc -std=c11 -Wall -fmax-errors=10 -Wextra (... you need to complete this line correctly ...)
-*/
 
 int main(void) {
 
     int num;
     double resultsqrt;
     scanf("%d", &num);
-
+    
     resultsqrt = sqrt(num);
     printf("%.8lf\n", resultsqrt);
     printf("%.10lf\n", exp(1));
